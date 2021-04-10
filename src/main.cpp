0a7e7136accfd4913e8ac34a76b4a6d26fef9aa9
Ethan Bellmer <e.bellmer@outlook.com>
2021-04-10 20:18:41 +0100

    Remove patterns & use espalexa for array control

--- src/main.cpp
+++ src/main.cpp
@@ -1,137 +1,102 @@
 #include <Arduino.h>
-
 #include <FastLED.h>
 
-FASTLED_USING_NAMESPACE
-
-// FastLED "100-lines-of-code" demo reel, showing just a few 
-// of the kinds of animation patterns you can quickly and easily 
-// compose using FastLED.  
-//
-// This example also shows one easy way to define multiple 
-// animations patterns and have them automatically rotate.
-//
-// -Mark Kriegsman, December 2014
-
-#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
-#warning "Requires FastLED 3.1 or later; check github for latest code."
+#ifdef ARDUINO_ARCH_ESP32
+	#include <WiFi.h>
+	#include <AsyncTCP.h>
+#else
+	#include <ESP8266WiFi.h>
+	#include <ESPAsyncTCP.h>
 #endif
 
-#define DATA_PIN    15
-#define LED_TYPE    WS2811
-#define COLOR_ORDER GRB
-#define NUM_LEDS    606
+#define ESPALEXA_ASYNC
+#include <Espalexa.h>
+
+#include "wifiCredentials.h"
+
+
+#define SERIAL_BAUDRATE     115200
+//#define DEBUG
+#ifdef DEBUG	// Test LED strip only has 21 LEDs so use that value for testing & debug.
+	#define NUM_LEDS		21
+	#define DEVICE_NAME		"Test Light"
+#else
+	#define NUM_LEDS		606
+	#define DEVICE_NAME		"Ring Light"
+#endif
+
+#define DATA_PIN			15
+#define LED_TYPE			WS2811
+#define COLOR_ORDER			GRB
+#define FRAMES_PER_SECOND	120
+
 
 CRGB leds[NUM_LEDS];
-
-#define BRIGHTNESS          255
-#define FRAMES_PER_SECOND   120
+Espalexa espalexa;
 
 
-void nextPattern();
-void addGlitter( fract8 chanceOfGlitter) ;
-void rainbow();
-void rainbowWithGlitter();
-void confetti();
-void sinelon();
-void juggle();
-void bpm();
+//callback function prototype
+void wifiSetup();
+void colorLightChanged(uint8_t brightness, uint32_t rgb);
+void setBrightness(uint8_t brightness);
+void setSolidColour(CRGB Colour, uint8_t brightness);
 
-void setup() {
-  delay(3000); // 3 second delay for recovery
-  
-  // tell FastLED about the LED strip configuration
-  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 
-  // set master brightness control
-  FastLED.setBrightness(BRIGHTNESS);
+void setup()
+{
+	delay(3000); // 3 second delay for recovery
+	
+	Serial.begin(SERIAL_BAUDRATE);
+
+	FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);	// tell FastLED about the LED strip configuration
+	setBrightness(0);	// Set brightness to 0, so LED array will default to off.
+
+	wifiSetup();
+
+	espalexa.addDevice(DEVICE_NAME, colorLightChanged);
+	espalexa.begin();
 }
 
-
-// List of patterns to cycle through.  Each is defined as a separate function below.
-typedef void (*SimplePatternList[])();
-// SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
-SimplePatternList gPatterns = { rainbow };
-
-uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
-uint8_t gHue = 0; // rotating "base color" used by many of the patterns
-  
 void loop()
 {
-  // Call the current pattern function once, updating the 'leds' array
-  gPatterns[gCurrentPatternNumber]();
+	espalexa.loop();
 
-  // send the 'leds' array out to the actual LED strip
-  FastLED.show();  
-  // insert a delay to keep the framerate modest
-  FastLED.delay(1000/FRAMES_PER_SECOND); 
-
-  // do some periodic updates
-  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+	FastLED.show();	// send the 'leds' array out to the actual LED strip
+	FastLED.delay(1000/FRAMES_PER_SECOND);	// insert a delay to keep the framerate modest
 }
 
-#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
-
-void nextPattern()
+void wifiSetup()
 {
-  // add one to the current pattern number, and wrap around at the end
-  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+	// Set WIFI module to STA mode
+	WiFi.mode(WIFI_STA);
+
+	// Connect
+	Serial.printf("[WIFI] Connecting to %s ", WIFI_SSID);
+	WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+	// Wait
+	while (WiFi.status() != WL_CONNECTED) {
+		Serial.print(".");
+		delay(100);
+	}
+	Serial.println();
+	Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
 }
 
-void rainbow() 
+void colorLightChanged(uint8_t brightness, uint32_t rgb)
 {
-  // FastLED's built-in rainbow generator
-  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+	setSolidColour(CRGB((rgb >> 16) & 0xFF, (rgb >>  8) & 0xFF, rgb & 0xFF), brightness);
 }
 
-void rainbowWithGlitter() 
+void setBrightness(uint8_t brightness)
 {
-  // built-in FastLED rainbow, plus some random sparkly glitter
-  rainbow();
-  addGlitter(80);
+	// set master brightness control	//	255 maximum value	127 half
+	FastLED.setBrightness(brightness);
 }
 
-void addGlitter( fract8 chanceOfGlitter) 
+void setSolidColour(CRGB colour, uint8_t brightness)
 {
-  if( random8() < chanceOfGlitter) {
-    leds[ random16(NUM_LEDS) ] += CRGB::White;
-  }
-}
-
-void confetti() 
-{
-  // random colored speckles that blink in and fade smoothly
-  fadeToBlackBy( leds, NUM_LEDS, 10);
-  int pos = random16(NUM_LEDS);
-  leds[pos] += CHSV( gHue + random8(64), 200, 255);
-}
-
-void sinelon()
-{
-  // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
-  leds[pos] += CHSV( gHue, 255, 192);
-}
-
-void bpm()
-{
-  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
-  CRGBPalette16 palette = PartyColors_p;
-  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
-  for( int i = 0; i < NUM_LEDS; i++) { //9948
-    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
-  }
-}
-
-void juggle() {
-  // eight colored dots, weaving in and out of sync with each other
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  byte dothue = 0;
-  for( int i = 0; i < 8; i++) {
-    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
+	fadeToBlackBy(leds, NUM_LEDS, 10);
+	setBrightness(brightness);
+	fill_solid(leds, NUM_LEDS, colour);
 }
